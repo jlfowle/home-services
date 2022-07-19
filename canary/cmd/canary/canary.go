f6e7c1d5a9fdcc2be8ff4de0b0c123d21d9cc713
Jasph Fowler <jasph.fowler@gmail.com>
2022-07-19 07:14:37 -0500

    no grpc for now

--- canary/cmd/canary/canary.go
+++ canary/cmd/canary/canary.go
@@ -8,27 +8,22 @@ import (
 	"os/signal"
 	"syscall"
 
-	pb "github.com/jlfowle/home-services/canary/api/v1/canary"
 	"github.com/jlfowle/home-services/canary/pkg/canary"
 	"github.com/jlfowle/home-services/canary/pkg/canary/endpoints"
 	"github.com/jlfowle/home-services/canary/pkg/canary/transport"
 
-	"github.com/go-kit/kit/log"
-	kitgrpc "github.com/go-kit/kit/transport/grpc"
+	"github.com/go-kit/log"
 	"github.com/oklog/oklog/pkg/group"
-	"google.golang.org/grpc"
 )
 
 const (
 	defaultHTTPPort = "8081"
-	defaultGRPCPort = "8082"
 )
 
 func main() {
 	var (
 		logger   log.Logger
 		httpAddr = net.JoinHostPort("localhost", envString("HTTP_PORT", defaultHTTPPort))
-		grpcAddr = net.JoinHostPort("localhost", envString("GRPC_PORT", defaultGRPCPort))
 	)
 
 	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
@@ -38,7 +33,6 @@ func main() {
 		service     = canary.NewService()
 		eps         = endpoints.NewEndpointSet(service)
 		httpHandler = transport.NewHTTPHandler(eps)
-		grpcServer  = transport.NewGRPCServer(eps)
 	)
 
 	var g group.Group
@@ -46,32 +40,14 @@ func main() {
 		// The HTTP listener mounts the Go kit HTTP handler we created.
 		httpListener, err := net.Listen("tcp", httpAddr)
 		if err != nil {
-			logger.Log("transport", "HTTP", "during", "Listen", "err", err)
+			_ = logger.Log("transport", "HTTP", "during", "Listen", "err", err)
 			os.Exit(1)
 		}
 		g.Add(func() error {
-			logger.Log("transport", "HTTP", "addr", httpAddr)
+			_ = logger.Log("transport", "HTTP", "addr", httpAddr)
 			return http.Serve(httpListener, httpHandler)
 		}, func(error) {
 			httpListener.Close()
-		})
-	}
-	{
-		// The gRPC listener mounts the Go kit gRPC server we created.
-		grpcListener, err := net.Listen("tcp", grpcAddr)
-		if err != nil {
-			logger.Log("transport", "gRPC", "during", "Listen", "err", err)
-			os.Exit(1)
-		}
-		g.Add(func() error {
-			logger.Log("transport", "gRPC", "addr", grpcAddr)
-			// we add the Go Kit gRPC Interceptor to our gRPC service as it is used by
-			// the here demonstrated zipkin tracing middleware.
-			baseServer := grpc.NewServer(grpc.UnaryInterceptor(kitgrpc.Interceptor))
-			pb.RegisterCanaryServer(baseServer, grpcServer)
-			return baseServer.Serve(grpcListener)
-		}, func(error) {
-			grpcListener.Close()
 		})
 	}
 	{
@@ -90,7 +66,7 @@ func main() {
 			close(cancelInterrupt)
 		})
 	}
-	logger.Log("exit", g.Run())
+	_ = logger.Log("exit", g.Run())
 }
 
 func envString(env, fallback string) string {
