d9707800bf52c99b7887d12a9f924a3b4876d26d
Jasph Fowler <65310191+jlfowle@users.noreply.github.com>
2023-04-15 16:07:38 +0000

    reduce duplication in testing

--- service-kit/healthz/healthz_test.go
+++ service-kit/healthz/healthz_test.go
@@ -2,15 +2,12 @@ package healthz_test
 
 import (
 	"errors"
+	"fmt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/jlfowle/home-services/service-kit/healthz"
-)
-
-var (
-	dummyService healthz.HealthService
 )
 
 type mockTest struct {
@@ -25,305 +22,101 @@ func (m *mockTest) Run() error {
 }
 
 var _ = Describe("Healthz", func() {
-	Context("with all passing checks", func() {
-		BeforeEach(func() {
-			dummyService = healthz.NewHealthService(
-				[]healthz.Check{
-					{Name: "ready", Test: &mockTest{}},
-				},
-				[]healthz.Check{
-					{Name: "live", Test: &mockTest{}},
-				},
-			)
-		})
-		It("Is healthy", func() {
-			report := dummyService.GetHealth()
-			Expect(report.Healthy).To(BeTrue())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(2))
-			for _, t := range report.TestResults {
-				Expect(t.Err).ToNot(HaveOccurred())
-				Expect(t.Pass).To(BeTrue())
-			}
-		})
-		It("Is ready", func() {
-			report := dummyService.GetReady()
-			Expect(report.Healthy).To(BeTrue())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).ToNot(HaveOccurred())
-				Expect(t.Pass).To(BeTrue())
-			}
-		})
-		It("Is live", func() {
-			report := dummyService.GetLive()
-			Expect(report.Healthy).To(BeTrue())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).ToNot(HaveOccurred())
-				Expect(t.Pass).To(BeTrue())
-			}
-		})
-	})
-	Context("with all failing checks", func() {
-		BeforeEach(func() {
-			dummyService = healthz.NewHealthService(
-				[]healthz.Check{
-					{Name: "ready", Test: &mockTest{shouldFail: true}},
-				},
-				[]healthz.Check{
-					{Name: "live", Test: &mockTest{shouldFail: true}},
-				},
-			)
-		})
-		It("Is healthy", func() {
-			report := dummyService.GetHealth()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(2))
-			for _, t := range report.TestResults {
-				Expect(t.Err).To(HaveOccurred())
-				Expect(t.Pass).To(BeFalse())
-				Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-			}
-		})
-		It("Is ready", func() {
-			report := dummyService.GetReady()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).To(HaveOccurred())
-				Expect(t.Pass).To(BeFalse())
-				Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-			}
-		})
-		It("Is live", func() {
-			report := dummyService.GetLive()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).To(HaveOccurred())
-				Expect(t.Pass).To(BeFalse())
-				Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-			}
-		})
-	})
-	Context("with failing ready but passing live", func() {
-		BeforeEach(func() {
-			dummyService = healthz.NewHealthService(
-				[]healthz.Check{
-					{Name: "ready", Test: &mockTest{shouldFail: true}},
-				},
-				[]healthz.Check{
-					{Name: "live", Test: &mockTest{}},
-				},
-			)
-		})
-		It("Is healthy", func() {
-			report := dummyService.GetHealth()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(2))
-			passes := 0
-			fails := 0
-			for _, t := range report.TestResults {
-				if t.Pass {
-					Expect(t.Err).ToNot(HaveOccurred())
-					passes++
-				} else {
-					Expect(t.Err).To(HaveOccurred())
-					Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-					fails++
-				}
-			}
-			Expect(passes).To(BeEquivalentTo(1))
-			Expect(fails).To(BeEquivalentTo(1))
-		})
-		It("Is ready", func() {
-			report := dummyService.GetReady()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).To(HaveOccurred())
-				Expect(t.Pass).To(BeFalse())
-				Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-			}
-		})
-		It("Is live", func() {
-			report := dummyService.GetLive()
-			Expect(report.Healthy).To(BeTrue())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).ToNot(HaveOccurred())
-				Expect(t.Pass).To(BeTrue())
-			}
-		})
-	})
-	Context("with failing live but passing ready", func() {
-		BeforeEach(func() {
-			dummyService = healthz.NewHealthService(
-				[]healthz.Check{
-					{Name: "ready", Test: &mockTest{}},
-				},
-				[]healthz.Check{
-					{Name: "live", Test: &mockTest{shouldFail: true}},
-				},
-			)
-		})
-		It("Is healthy", func() {
-			report := dummyService.GetHealth()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(2))
-			passes := 0
-			fails := 0
-			for _, t := range report.TestResults {
-				if t.Pass {
-					Expect(t.Err).ToNot(HaveOccurred())
-					passes++
-				} else {
-					Expect(t.Err).To(HaveOccurred())
-					Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-					fails++
-				}
-			}
-			Expect(passes).To(BeEquivalentTo(1))
-			Expect(fails).To(BeEquivalentTo(1))
-		})
-		It("Is ready", func() {
-			report := dummyService.GetReady()
-			Expect(report.Healthy).To(BeTrue())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).ToNot(HaveOccurred())
-				Expect(t.Pass).To(BeTrue())
-			}
-		})
-		It("Is live", func() {
-			report := dummyService.GetLive()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).To(HaveOccurred())
-				Expect(t.Pass).To(BeFalse())
-				Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-			}
-		})
-	})
-	Context("with some failing health", func() {
-		BeforeEach(func() {
-			dummyService = healthz.NewHealthService(
-				[]healthz.Check{
-					{Name: "ready", Test: &mockTest{}},
-					{Name: "ready", Test: &mockTest{shouldFail: true}},
-				},
-				[]healthz.Check{
-					{Name: "live", Test: &mockTest{}},
-				},
-			)
-		})
-		It("Is healthy", func() {
-			report := dummyService.GetHealth()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(3))
-			passes := 0
-			fails := 0
-			for _, t := range report.TestResults {
-				if t.Pass {
-					Expect(t.Err).ToNot(HaveOccurred())
-					passes++
-				} else {
-					Expect(t.Err).To(HaveOccurred())
-					Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-					fails++
-				}
-			}
-			Expect(passes).To(BeEquivalentTo(2))
-			Expect(fails).To(BeEquivalentTo(1))
-		})
-		It("Is ready", func() {
-			report := dummyService.GetReady()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(2))
-			passes := 0
-			fails := 0
-			for _, t := range report.TestResults {
-				if t.Pass {
-					Expect(t.Err).ToNot(HaveOccurred())
-					passes++
-				} else {
-					Expect(t.Err).To(HaveOccurred())
-					Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-					fails++
-				}
-			}
-			Expect(passes).To(BeEquivalentTo(1))
-			Expect(fails).To(BeEquivalentTo(1))
-		})
-		It("Is live", func() {
-			report := dummyService.GetLive()
-			Expect(report.Healthy).To(BeTrue())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).ToNot(HaveOccurred())
-				Expect(t.Pass).To(BeTrue())
-			}
-		})
-	})
-	Context("with some failing health", func() {
-		BeforeEach(func() {
-			dummyService = healthz.NewHealthService(
-				[]healthz.Check{
-					{Name: "ready", Test: &mockTest{}},
-				},
-				[]healthz.Check{
-					{Name: "live1", Test: &mockTest{}},
-					{Name: "live2", Test: &mockTest{shouldFail: true}},
-				},
-			)
-		})
-		It("Is healthy", func() {
-			report := dummyService.GetHealth()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(3))
-			passes := 0
-			fails := 0
-			for _, t := range report.TestResults {
-				if t.Pass {
-					Expect(t.Err).ToNot(HaveOccurred())
-					passes++
-				} else {
-					Expect(t.Err).To(HaveOccurred())
-					Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-					fails++
-				}
-			}
-			Expect(passes).To(BeEquivalentTo(2))
-			Expect(fails).To(BeEquivalentTo(1))
-		})
-		It("Is ready", func() {
-			report := dummyService.GetReady()
-			Expect(report.Healthy).To(BeTrue())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(1))
-			for _, t := range report.TestResults {
-				Expect(t.Err).ToNot(HaveOccurred())
-				Expect(t.Pass).To(BeTrue())
-			}
-		})
-		It("Is live", func() {
-			report := dummyService.GetLive()
-			Expect(report.Healthy).To(BeFalse())
-			Expect(len(report.TestResults)).To(BeEquivalentTo(2))
-			passes := 0
-			fails := 0
-			for _, t := range report.TestResults {
-				if t.Pass {
-					Expect(t.Err).ToNot(HaveOccurred())
-					passes++
-				} else {
-					Expect(t.Err).To(HaveOccurred())
-					Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
-					fails++
-				}
-			}
-			Expect(passes).To(BeEquivalentTo(1))
-			Expect(fails).To(BeEquivalentTo(1))
-		})
-	})
+	var (
+		dummyService healthz.HealthService
+		tests []map[string][]bool = []map[string][]bool{
+			{
+				"ready_checks": []bool{false},
+				"live_checks": []bool{false},
+			},
+			{
+				"ready_checks": []bool{true},
+				"live_checks": []bool{false},
+			},
+			{
+				"ready_checks": []bool{false},
+				"live_checks": []bool{true},
+			},
+			{
+				"ready_checks": []bool{false, true},
+				"live_checks": []bool{false},
+			},
+			{
+				"ready_checks": []bool{false},
+				"live_checks": []bool{false, true},
+			},
+		}
+	)
+
+	for _, test := range tests {
+		var (
+			readyChecks, liveChecks []healthz.Check
+			count, expectedPasses, expectedFails int
+		)
+		for _, checkType := range []string{"health", "ready", "live"} {
+			Describe("test health checks", func(){
+				BeforeEach(func() {
+					for i, c := range test["ready_checks"] {
+						if checkType != "live" {
+							count++
+							if c {
+								expectedFails++
+							} else {
+								expectedPasses++
+							}
+						}
+						readyChecks = append(readyChecks, healthz.Check{Name: fmt.Sprintf("ready%v", i), Test: &mockTest{shouldFail: c}})
+					}
+					for i, c := range test["live_checks"] {
+						if checkType != "ready" {
+							count++
+							if c {
+								expectedFails++
+							} else {
+								expectedPasses++
+							}
+						}
+						liveChecks = append(liveChecks, healthz.Check{Name: fmt.Sprintf("live%v", i), Test: &mockTest{shouldFail: c}})
+					}
+					dummyService = healthz.NewHealthService(
+						readyChecks,
+						liveChecks,
+					)
+				})
+				Describe("check report", func() {
+					It("reports correctly", func() {
+						var report *healthz.Report
+						if checkType == "health" {
+							report = dummyService.GetHealth()
+						} else if checkType == "ready" {
+							report = dummyService.GetReady()
+						} else if checkType == "live" {
+							report = dummyService.GetLive()
+						}
+						if expectedFails > 0 {
+							Expect(report.Healthy).To(BeFalse())
+						} else {
+							Expect(report.Healthy).To(BeTrue())
+						}
+						
+						Expect(len(report.TestResults)).To(BeEquivalentTo(count))
+						passes := 0
+						fails := 0
+						for _, t := range report.TestResults {
+							if t.Pass {
+								Expect(t.Err).ToNot(HaveOccurred())
+								passes++
+							} else {
+								Expect(t.Err).To(HaveOccurred())
+								Expect(errors.Unwrap(t.Err)).To(HaveOccurred())
+								fails++
+							}
+						}
+						Expect(passes).To(BeEquivalentTo(expectedPasses))
+						Expect(fails).To(BeEquivalentTo(expectedFails))
+					})
+				})
+			})
+		}
+	}
 })
