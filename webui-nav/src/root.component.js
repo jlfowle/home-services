a17998ef9cc4e3b215458a1f2b64a21ada63d30c
Jasph Fowler <jasph.fowler@gmail.com>
2023-04-20 06:02:02 -0500

    initial components

---
+++ webui-nav/src/root.component.js
@@ -0,0 +1,3 @@
+export default function Root(props) {
+  return <div><div id="mySidenav" class="sidenav"><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a><a href="#">About</a><a href="#">Services</a><a href="#">Clients</a><a href="#">Contact</a></div><span onclick="openNav()">open</span></div>;
+}
